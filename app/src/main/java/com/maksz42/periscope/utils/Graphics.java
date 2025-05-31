8a763add536aff78ac6fbea668b74909731f2d75
maksz42 <48691053+maksz42@users.noreply.github.com>
2025-05-31 12:14:47 +0200

    Another micro-optimization
    just for fun

--- app/src/main/java/com/maksz42/periscope/utils/Graphics.java
+++ app/src/main/java/com/maksz42/periscope/utils/Graphics.java
@@ -6,19 +6,23 @@ public final class Graphics {
   private Graphics() { }
 
   public static void scaleRectKeepRatio(int srcW, int srcH, int limitW, int limitH, Rect dstRect) {
-    int dstW;
-    int dstH;
+    int dstW, dstH;
+    int left, top, right, bottom;
     if (srcW * limitH > srcH * limitW) {
       // width limit
-      dstW = limitW;
       dstH = (srcH * limitW) / srcW;
+      left = 0;
+      top = (limitH - dstH) >>> 1;
+      right = limitW;
+      bottom = top + dstH;
     } else {
       // height limit
-      dstH = limitH;
       dstW = (srcW * limitH) / srcH;
+      left = (limitW - dstW) >>> 1;
+      top = 0;
+      right = left + dstW;
+      bottom = limitH;
     }
-    int left = (limitW - dstW) >>> 1;
-    int top = (limitH - dstH) >>> 1;
-    dstRect.set(left, top, left + dstW, top + dstH);
+    dstRect.set(left, top, right, bottom);
   }
 }
