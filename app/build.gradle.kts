07a54cceedd62320e475564212fa6b9deea00e69
maksz42 <48691053+maksz42@users.noreply.github.com>
2025-12-28 17:26:14 +0100

    Release v0.19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         // TODO api 35
         // forces edge-to-edge
         targetSdk = 34
-        versionCode = 18
-        versionName = "0.18"
+        versionCode = 19
+        versionName = "0.19"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
